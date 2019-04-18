7ebe4bc471041104a41ff192b96ce3e702daa1f8
zhangwen <18514430012@163.com>
2019-04-18 15:54:47 +0800

    提交tp5框架

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,66 @@
+<?php
+namespace app\admin\controller;
+
+use  think\Controller;
+use  think\config;
+use  think\Db;
+use  think\Session;
+
+class Login extends Controller
+{
+	/**
+	  * 登陆
+	*/
+	public function login(){
+		$uname = input('post.uname');
+		$upwd = md5(input('post.upwd'));
+		$thirdSession = input('post.thirdSession');
+		$isset_session = Session::has('openid');
+		$openid = Session::get('openid');
+		$result = Db::table('user')->field('uid,uname')->where(['uname'=>$uname,'upwd'=>$upwd])->find();
+		if( $result ){
+			Db::table('user')->where('openid',$openid)->update(['openid'=>'']);
+			Db::table('user')->where('uid',$result['uid'])->update(['openid'=>$openid]);
+			return json(['code'=>'1','message'=>'登录成功',"result"=>$result['role_id']]);
+		}else{
+			return json(['code'=>'2','message'=>'登录失败',"result"=>'']);
+		}
+	}
+
+	/**
+	  * 退出登陆
+	*/
+	public function backlogin(){
+		$thirdSession = input('post.thirdSession');
+		$isset_session = Session::has('openid');
+		if( $isset_session ){
+			$openid = Session::get('openid');
+			$result = Db::table('user')->where('openid',$openid)->update(['openid'=>null]);
+			if( $result ){
+				return json(['code'=>'1','message'=>'退出成功',"result"=>$result['uname']]);
+			}else{
+				return json(['code'=>'2','message'=>'退出失败',"result"=>'']);
+			}
+		} 
+	}
+
+	/**
+	  * 检测是否绑定
+	*/
+	public function opuser(){
+		$thirdSession = input('post.thirdSession');
+		$isset_session = Session::has('openid');
+		$openid = Session::get('openid');
+		if($isset_session){
+			$result = Db::table('user')->field('uname')->where('openid',$openid)->find();
+			if( !empty( $result ) ){
+				return json(['code'=>'1001','message'=>'已绑定',"result"=>$result['uname']]);
+			}else{
+				return json(['code'=>'1002','message'=>'未绑定',"result"=>'']);
+			}
+		}else{
+			return json(['code'=>'2001','message'=>'未登录',"result"=>'']);
+		}
+		
+	}
+}
---
+++ application/admin/controller/Wx.php
@@ -0,0 +1,80 @@
+<?php
+namespace app\admin\controller;
+
+use Wxdev\WXBizDataCrypt;
+use think\config;
+use think\Session;
+use think\Db;
+
+class Wx
+{
+    public function index()
+    {
+    	$code = input('get.code');
+        //appid
+		$APPID = config::get("wechat.wx_appid");
+		//appscret
+		$AppSecret = config::get("wechat.wx_appsecret"); 
+		//接口域名
+		$wx_request_url = config::get("wechat.wx_request_url"); 
+		//调用微信接口参数
+		$param = array( 
+	        'appid' => $APPID, 
+	        'secret' => $AppSecret, 
+	        'js_code' => $code, 
+	        'grant_type' => 'authorization_code'
+	      ); 
+		$arr = http_send($wx_request_url, $param, 'post'); 
+		$arr = json_decode($arr,true);
+		if(isset($arr['errcode']) && !empty($arr['errcode'])){
+		  return json(['code'=>'2','message'=>$arr['errmsg'],"result"=>null]);
+		}
+		$openid = $arr['openid'];
+		$session_key = $arr['session_key'];
+
+		// 数据签名校验
+		$signature = input("signature");
+		$signature2 = sha1($_GET['rawData'].$session_key);  //框架自带的input会过滤掉必要的数据
+		if ($signature != $signature2) {
+		  $msg = "shibai 1";
+		  return json(['code'=>'2','message'=>'获取失败',"result"=>$msg]);
+		}
+
+		//开发者如需要获取敏感数据，需要对接口返回的加密数据( encryptedData )进行对称解密
+		$encryptedData = $_GET['encryptedData'];
+		$iv = $_GET['iv'];
+		include_once (EXTEND_PATH. 'Wxdev/wxBizDataCrypt.php');
+		$pc = new \WXBizDataCrypt($APPID, $session_key);
+		$errCode = $pc->decryptData($encryptedData, $iv, $data);  //其中$data包含用户的所有数据
+		if ($errCode != 0) {
+		  return json(['code'=>'2','message'=>'获取失败',"result"=>null]);
+		}
+		//生成第三方3rd_session
+		$session3rd  = null;
+		$strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+		$max = strlen($strPol)-1;
+		for($i=0;$i<16;$i++){
+		  $session3rd .=$strPol[rand(0,$max)];
+		}
+		//设置session
+		Session::set('openid',$openid);
+		$openid = Session::get('openid');
+		return json(['code'=>'1','message'=>$openid,"result"=>$openid]);
+
+    }
+
+    public function gettype(){
+    	$serch = input('post.serch');
+    	$type = Db::table('type')->where('type_pid','1')->select();
+    	foreach( $type as $k=>&$v ){
+    		$map['type_pid'] = array('eq',$v['type_id']);
+    		if($serch != ''){
+    			$map['type_name'] = array('like','%'.$serch.'%');
+    		}
+    		$child = Db::table('type')->where($map)->select();
+    		$v['children'] = $child;
+    		$v['ishaveChild'] = empty($child)?false:true;
+    	}
+    	return json_encode(['code'=>'1','message'=>'获取成功',"result"=>$type]);
+    }
+}
